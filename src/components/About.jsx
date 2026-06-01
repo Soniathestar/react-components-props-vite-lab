860dc7d621462e76917733795d08a04eb5612f49
Sonia <soniangaruiya@gmail.com>
2026-06-01 12:43:25 +0300

    Set default image for About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-function About({ image, about }) {
+function About({ image = "https://via.placeholder.com/215", about }) {
   return (
     <aside>
       <img src={image} alt="blog logo" />
@@ -7,4 +7,4 @@ function About({ image, about }) {
   );
 }
 
-export default About; 
+export default About;
